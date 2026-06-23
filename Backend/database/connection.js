94825039e32339d47ece96a30a5da57ef1ccf809
Angelo <angelo@MacBook-Air-de-Angelo.local>
2026-06-22 19:55:58 -0600

    Estructura y conexion base de datos

---
+++ Backend/database/connection.js
@@ -0,0 +1,22 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+require('dotenv').config();
+
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+
+const bd = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  }
+);
+
+module.exports = bd;
---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config(); // llama a dotenv para que lea el archivo .env y cargue las variables de entorno
+
+const express = require('express'); // llama a express para crear el servidor
+const morgan = require('morgan');// llama a morgan para registrar las solicitudes HTTP en la consola
+
+const sequelize = require('./database/connection'); // llama a la conexión de la base de datos
+const app = express(); // crea una instancia de express
+const PORT = process.env.PORT || 3000; // define el puerto en el que se ejecutará el servidor, si no se define en el archivo .env, se usará el puerto 3000 por defecto
+
+sequelize.sync(
+    { force: false }
+).then(() => {
+    console.log('Base de datos conectada');
+}).catch((error) => {
+    console.error('Base de datos no esta conectada:', error);
+    });
+
+app.use(morgan('dev')); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Define your routes here
+// Example: app.use('/api', require('./routes/api'));
+
+app.listen(PORT, () => {
+    console.log(`Servidor esta corriendo en el puerto ${PORT}`);
+}); 
+
