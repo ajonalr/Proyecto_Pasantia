c693b5407ddb9e8d05ab5c09f27de0f1d4c923d3
Ariel Ramirez <ariel12jona@gmail.com>
2026-06-26 19:09:45 -0600

    Implement JWT authentication middleware and update article creation routes

--- Backend/routes/articulo.routes.js
+++ Backend/routes/articulo.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const articuloControllers = require('../controllers/articulo.controllers');
 const { validarStock } = require('../middlewares/articulo.middleware')
+const  authGuard  = require('../middlewares/jwt.guard');
 
 router.get('/', articuloControllers.getAllArticulos);
 router.get('/:codigo', articuloControllers.getArticuloById);
 // Usar validarStock para crear artículos
-router.post('/', validarStock, articuloControllers.createArticulo);
+router.post('/', authGuard, articuloControllers.createArticulo);
+router.post('/store', authGuard, articuloControllers.createArticulo);
 router.put('/:codigo', articuloControllers.updateArticulo);
 router.delete('/:codigo', articuloControllers.deleteArticulo);
 
